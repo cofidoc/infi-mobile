6c8c6af11411748e52167b39581edf064b448899
superchauzette <kevin.tillot@gmail.com>
2021-09-20 15:49:04 +0200

    init

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2Knt8upk5xFch4aq0ALxT2OoIpc7ZgWE",
+  authDomain: "infi-mobile.firebaseapp.com",
+  projectId: "infi-mobile",
+  storageBucket: "infi-mobile.appspot.com",
+  messagingSenderId: "1042737341632",
+  appId: "1:1042737341632:web:f9d53e3994d1718dcf7925"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
